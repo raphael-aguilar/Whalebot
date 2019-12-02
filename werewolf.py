22935566ba689948160cbebed8426f6f2281cb38
Martin <martdr123@gmail.com>
2019-12-02 19:59:31 +1100

    Just a bit of a skeleton of some classes to start, with some notes. We don't have to use the code, mostly just getting the feel of GitHub

---
+++ werewolf.py
@@ -0,0 +1,21 @@
+
+class werewolf_game:
+    """The class for the game engine.
+    Will need to know the players (by discord ID # and name)
+    And the ruleset - the roles in play, how many of each, and any modifiers"""
+
+    pass
+
+class player:
+    """For each player in the game, defined by discord ID # and name.
+    Each player will have a role attribute"""
+
+    pass
+
+class role:
+    """The role for each player, with its own rules.
+    Roles will have attributes & methods: team, team_appearance (eg. lycan), win_condition,
+    first_night_behaviour, night_behaviour, night_order_precedence
+
+    Some roles may have attributes modifiable for different rulesets,
+    eg. bodyguard, can be able to protect self/not, can protect same person twice/not"""
