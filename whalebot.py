d15af8375bde3fad4167817e92dfaf65879e0971
Raphael Aguilar <raphaela@student.unimelb.edu.au>
2019-12-02 23:44:03 +1100

    gitignore and bot  added

---
+++ whalebot.py
@@ -0,0 +1,12 @@
+import discord
+
+
+def read_token():
+    with open("token.txt", "r") as f:
+        lines = f.readlines()
+        return lines[0].strip()
+
+token = read_token()
+
+client = discord.Client()
+client.run(token)
