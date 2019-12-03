cc6c7c34e758f19bb49c2cd3c02ffa1561b2e186
Martin <martdr123@gmail.com>
2019-12-03 23:54:36 +1100

    Added number_role_alive, kill. Added Role subclass attributes. Assignment of roles now assigns instances of relevant role objects, not just strings. Also added a test game state.

--- werewolf.py
+++ werewolf.py
@@ -7,6 +7,8 @@ class WerewolfGame:
     Will need to know the players (by discord ID # and name)
     And the ruleset - the roles in play, how many of each, and any modifiers"""
 
+    # SETUP
+
     def __init__(self, player_list, ruleset=None):
         """Takes a list of tuples for players, the tuples will be (player_id, player_name)"""
 
@@ -19,13 +21,12 @@ class WerewolfGame:
         # probably a dict of {'role':{modifier1:value, modifier2:value},...}, with each role in play included just once
         # most will just be empty/default, these can be the arguments for when the roles are initialised
         # self.role_modifiers = {}
-        
+
         # list of roles in play, with each role occurring as many times as players with that role
         self.role_list = []
 
     def add_player(self, player_id, player_name):
-        """Add a new player to the game
-        Separate of __init__ so that this can be called separately"""
+        """Add a new player to the game"""
 
         self.players[player_id] = Player(player_id, player_name)
 
@@ -39,6 +40,22 @@ class WerewolfGame:
         for player in self.players.values():
             player.assign_role(shuffled_role_list.pop())
 
+        ##To implement: lists of players of each role, eg. werewolves = []
+
+    # GAMEPLAY
+
+    def number_role_alive(self, role_name):
+        """Returns an int, the number of remaining players of the role specified that are still alive"""
+        ##To implement: make this function more general, eg. number_alive(role='Werewolf'), number_alive(team=...)
+
+        n = 0
+
+        for player in self.players.values():
+            if player.role.name == role_name:
+                n += player.alive
+
+        return n
+
 
 class Player:
     """For each player in the game, defined by discord ID # and name.
@@ -48,10 +65,24 @@ class Player:
         self.id = player_id
         self.name = player_name
 
+        self.alive = True
+
         self.role = None
 
-    def assign_role(self, role):
-        self.role = role
+    def assign_role(self, role_name):
+        """Updates the player's role, to an instance of the relevant role object"""
+
+        # A dictionary of roles that are present, keys are names as strings, values are the role class objects
+        available_roles = {cls.__name__: cls for cls in Role.__subclasses__()}
+
+        self.role = available_roles[role_name]()
+
+    def kill(self):
+        """Kills the player"""
+
+        self.alive = False
+
+        print(self.name, "has been killed, they were on the", self.role.team)
 
 
 class Role:
@@ -66,15 +97,37 @@ class Role:
 
 
 class Villager(Role):
+    """The villager role"""
 
-    pass
+    def __init__(self):
+        self.team = 'villager_team'
+        self.name = 'Villager'
+        self.win_condition = None
+        self.night_behaviour = None
 
 
 class Werewolf(Role):
+    """The werewolf role"""
 
-    pass
+    def __init__(self):
+        self.team = 'werewolf_team'
+        self.name = 'Werewolf'
+        self.win_condition = None
+        self.night_behaviour = None
 
 
 class Seer(Role):
+    """The seer role"""
 
-    pass
+    def __init__(self):
+        self.team = 'villager_team'
+        self.name = 'Seer'
+        self.win_condition = None
+        self.night_behaviour = None
+
+
+if __name__ == "__main__":
+    game = WerewolfGame([('#1', 'Raph'), ('#2', 'Martin'), ('#3', 'Louis'), ('#4', 'Tom')])
+    game.role_list = ['Villager', 'Villager', 'Seer', 'Werewolf']  # because not yet implemented in ruleset
+    game.assign_roles()
+
